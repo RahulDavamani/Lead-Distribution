9f954c1c1808d4b7c669d90fb9beca77784e2f39
Rahul Davamani <davamanirahul@gmail.com>
2023-12-09 05:09:11 +0530

    Rules Page

--- src/routes/rules/+page.svelte
+++ src/routes/rules/+page.svelte
@@ -1,4 +1,54 @@
 <script lang="ts">
+	import Icon from '@iconify/svelte';
+	import { trpc } from '../../trpc/client';
+	import { page } from '$app/stores';
+	import { trpcClientErrorHandler } from '../../trpc/trpcErrorhandler';
+	import { ui } from '../../stores/ui.store';
+	import { invalidateAll } from '$app/navigation';
+
 	export let data;
-	console.log(data);
+	$: ({ rules } = data);
 </script>
+
+<div class="container mx-auto my-10">
+	<div class="flex justify-between items-center">
+		<h1 class="text-3xl font-bold flex items-end gap-2">
+			Rules:
+			<span class="font-normal font-mono text-2xl">({rules.length})</span>
+		</h1>
+		<a href="/rules/rule-config" class="btn btn-sm btn-success">
+			<Icon icon="mdi:add" width={20} />
+			Add Rule
+		</a>
+	</div>
+	<div class="divider mt-1" />
+	<div class="flex flex-col gap-4">
+		{#each rules as { id, name }}
+			<div class="card border shadow p-4 flex flex-row justify-between">
+				<div>Rule Name</div>
+				<button
+					class="btn btn-sm btn-square btn-ghost btn-error"
+					on:click={async () => {
+						ui.setLoader({ title: 'Deleting Rule' });
+						await trpc($page).rule.deleteRole.query({ id }).catch(trpcClientErrorHandler);
+						ui.showToast({
+							class: 'alert-success',
+							title: 'Rule Deleted Successfully'
+						});
+						invalidateAll();
+						ui.setLoader();
+					}}
+				>
+					<Icon icon="mdi:delete" width={22} class="text-error" />
+				</button>
+			</div>
+		{:else}
+			<div class="text-center mt-10">No Rule Found</div>
+		{/each}
+	</div>
+	<!-- {#each rules as rule}
+      <div>
+         {rule.}
+      </div>
+   {/each} -->
+</div>
