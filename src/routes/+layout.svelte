796fb41870b57230e5f15060f286ecbdf7f2576e
Rahul Davamani <davamanirahul@gmail.com>
2023-12-18 14:40:08 +0530

    Auth Added for Rules Page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,17 @@
 	import Toast from './components/Toast.svelte';
 	import { ui } from '../stores/ui.store';
 	import '../../node_modules/.pnpm/flatpickr@4.6.13/node_modules/flatpickr/dist/flatpickr.css';
+	import { onMount } from 'svelte';
+	import { auth } from '../stores/auth.store';
+	import { page } from '$app/stores';
+
 	$: ({ loader } = $ui);
+	onMount(() => auth.authenticate());
+	$: error = (() => {
+		if (!$auth.isAuth) return { code: 401, message: 'Unauthorized' };
+		if ($page.url.pathname.startsWith('/rules') && !$auth.user?.userRole.find(({ ID }) => [5, 9].includes(ID)))
+			return { code: 403, message: 'Forbidden' };
+	})() as { code: number; message: string } | undefined;
 </script>
 
 <img src="https://bundlepublic.blob.core.windows.net/bundle/bundle_logo.png" alt="Logo" width="150" class="mb-4" />
@@ -16,7 +26,14 @@
 {/if}
 
 {#if !loader || loader.overlay !== false}
-	<slot />
+	{#if error}
+		<div class="card border shadow-lg py-6 px-2 max-w-sm mx-auto text-center mt-40">
+			<div class="text-4xl font-bold font-mono">{error.code}</div>
+			<div class="text-2xl">{error.message}</div>
+		</div>
+	{:else}
+		<slot />
+	{/if}
 {/if}
 
 <AlertModal />
---
+++ src/stores/auth.store.ts
@@ -0,0 +1,55 @@
+import { page } from '$app/stores';
+import { get, writable } from 'svelte/store';
+import { ui } from './ui.store';
+
+export interface Auth {
+	isAuth: boolean;
+	token?: string;
+	user?: {
+		Message: string;
+		UserKey: string;
+		CompanyKey: string;
+		userRole: { ID: number; Role: string }[];
+	};
+}
+
+export const auth = (() => {
+	const { subscribe, set, update } = writable<Auth>({ isAuth: false });
+
+	// Methods
+	const authenticate = async () => {
+		const { isAuth } = get(auth);
+		if (isAuth) return;
+
+		ui.setLoader({ title: 'Authenticating...' });
+		const $page = get(page);
+		const token = $page.url.searchParams.get('BPT');
+		if (!token) {
+			ui.setLoader();
+			return;
+		}
+
+		const validateTokenUrl = 'https://bundleapi.xyzies.com/Account/ValidateReportToken';
+		const res = await fetch(validateTokenUrl, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				AccessKey: 'FEEBD9BB-8A80-4AB5-938E-DD700468E06A',
+				Token: token
+			})
+		});
+
+		if (res.status === 200) {
+			const user = (await res.json()) as Auth['user'];
+			set({ isAuth: true, token, user });
+		}
+		ui.setLoader();
+	};
+
+	return {
+		subscribe,
+		set,
+		update,
+		authenticate
+	};
+})();
