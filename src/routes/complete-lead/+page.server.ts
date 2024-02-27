869ea6ed9bcf37533979cc3bce15b8c531b1714c
Rahul Davamani <davamanirahul@gmail.com>
2024-02-27 23:04:15 +0530

    Push lead straight into Completed

--- src/routes/complete-lead/+page.server.ts
+++ src/routes/complete-lead/+page.server.ts
@@ -1,11 +1,46 @@
 import { error } from '@sveltejs/kit';
 import { completeLead } from '../../trpc/routers/lead/helpers/lead.js';
+import prismaErrorHandler from '../../prisma/prismaErrorHandler.js';
+import { getCompanyKey } from '../../trpc/routers/lead/helpers/getCompanyKey.js';
 
 export const load = async (event) => {
 	const ProspectKey = event.url.searchParams.get('ProspectKey');
 	if (!ProspectKey) throw error(400, 'Bad Request: Missing params "ProspectKey"');
 
-	await completeLead({ ProspectKey, success: false, completeStatus: 'GHL Text Received' });
+	const lead = await prisma.ldLead
+		.findUnique({
+			where: { ProspectKey },
+			select: { id: true }
+		})
+		.catch(prismaErrorHandler);
+
+	if (lead) await completeLead({ ProspectKey, success: false, completeStatus: 'GHL Text Received' });
+	else {
+		const CompanyKey = await getCompanyKey(ProspectKey);
+		if (!CompanyKey) throw error(404, 'CompanyKey not found');
+		const rule = await prisma.ldRule
+			.findFirst({
+				where: { affiliates: { some: { CompanyKey } } },
+				select: {
+					id: true
+				},
+				orderBy: { createdAt: 'desc' }
+			})
+			.catch(prismaErrorHandler);
+		if (!rule) throw error(404, 'Rule not found');
+
+		await prisma.ldLeadCompleted.create({
+			data: {
+				ProspectKey,
+				ruleId: rule.id,
+				success: true,
+				completeStatus: 'Completed Lead from GHL',
+				logs: {
+					create: { log: 'Lead completed: Completed Lead from GHL' }
+				}
+			}
+		});
+	}
 
 	return {};
 };
