556f5c704b4c5e6dac5558a144da604d1217cf5c
Rahul Davamani <davamanirahul@gmail.com>
2024-02-29 23:17:07 +0530

    Queue Lead API

--- src/routes/queue-lead/+page.server.ts
+++ src/routes/queue-lead/+page.server.ts
@@ -1,19 +1,11 @@
 import { error } from '@sveltejs/kit';
 import prismaErrorHandler from '../../prisma/prismaErrorHandler.js';
-import { insertLeadCompleted } from '../../trpc/routers/lead/helpers/insertLeadCompleted.js';
-import { completeLead } from '../../trpc/routers/lead/helpers/completeLead.js';
+import { requeueLead } from '../../trpc/routers/lead/helpers/requeueLead.js';
+import { insertLead } from '../../trpc/routers/lead/helpers/insertLead.js';
 
 export const load = async (event) => {
 	const ProspectKey = event.url.searchParams.get('ProspectKey');
 	if (!ProspectKey) throw error(400, 'Bad Request: Missing params "ProspectKey"');
-
-	const completedLead = await prisma.ldLeadCompleted
-		.findUnique({
-			where: { ProspectKey },
-			select: { id: true }
-		})
-		.catch(prismaErrorHandler);
-	if (completedLead) throw error(400, 'Lead already in completed');
 
 	const lead = await prisma.ldLead
 		.findUnique({
@@ -21,9 +13,17 @@ export const load = async (event) => {
 			select: { id: true }
 		})
 		.catch(prismaErrorHandler);
+	if (lead) throw error(400, 'Lead already in Queue');
 
-	if (lead) await completeLead({ ProspectKey, success: false, completeStatus: 'GHL Text Received' });
-	else await insertLeadCompleted(ProspectKey);
+	const completeLead = await prisma.ldLeadCompleted
+		.findUnique({
+			where: { ProspectKey },
+			select: { id: true }
+		})
+		.catch(prismaErrorHandler);
+
+	if (completeLead) await requeueLead(ProspectKey);
+	else insertLead(ProspectKey);
 
 	return {};
 };
